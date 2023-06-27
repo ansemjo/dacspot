90847c90a9d0451b850e590a3e9461beab61e8e2
Anton Semjonov <anton@semjonov.de>
2023-06-27 12:52:36 +0200

    librespot: generate envs file from menu config

--- package/librespot/librespot.mk
+++ package/librespot/librespot.mk
@@ -10,7 +10,28 @@ LIBRESPOT_SITE = $(call github,librespot-org,librespot,$(LIBRESPOT_VERSION))
 LIBRESPOT_CARGO_BUILD_OPTS = $(call qstrip,$(BR2_PACKAGE_LIBRESPOT_BUILD_OPTS))
 
 define LIBRESPOT_INSTALL_INIT_SYSV
+
+	# install service file
 	$(INSTALL) -D -m 0755 $(LIBRESPOT_PKGDIR)S70librespot $(TARGET_DIR)/etc/init.d/
+
+	# create configuration file
+	mkdir -p $(TARGET_DIR)/etc/default/
+	echo "# librespot configuration" > $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_NAME=\"$(BR2_PACKAGE_LIBRESPOT_CONF_NAME)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_DEVICE_TYPE=\"$(BR2_PACKAGE_LIBRESPOT_CONF_DEVICE_TYPE)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_USERNAME=\"$(BR2_PACKAGE_LIBRESPOT_CONF_USERNAME)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_PASSWORD=\"$(BR2_PACKAGE_LIBRESPOT_CONF_PASSWORD)\"" >> $(TARGET_DIR)/etc/default/librespot
+	if [[ x$(BR2_PACKAGE_LIBRESPOT_CONF_DISABLE_DISCOVERY) == xy ]]; then \
+		echo "export LIBRESPOT_DISABLE_DISCOVERY=\"on\"" >> $(TARGET_DIR)/etc/default/librespot; \
+	fi
+	if [[ x$(BR2_PACKAGE_LIBRESPOT_CONF_AUTOPLAY) == xy ]]; then \
+		echo "export LIBRESPOT_AUTOPLAY=\"on\"" >> $(TARGET_DIR)/etc/default/librespot; \
+	fi
+	echo "export LIBRESPOT_CACHE=\"$(BR2_PACKAGE_LIBRESPOT_CONF_CACHE)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_VOLUME_CTRL=\"$(BR2_PACKAGE_LIBRESPOT_CONF_VOLUME_CTRL)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_BITRATE=\"$(BR2_PACKAGE_LIBRESPOT_CONF_BITRATE)\"" >> $(TARGET_DIR)/etc/default/librespot
+	echo "export LIBRESPOT_FORMAT=\"$(BR2_PACKAGE_LIBRESPOT_CONF_FORMAT)\"" >> $(TARGET_DIR)/etc/default/librespot
+
 endef
 
 $(eval $(cargo-package))
